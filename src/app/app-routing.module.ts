caa7fba1fb431e0ae291e679748fa38e1bf7ec0a
ReymonYo <Reymon_99@outlook.com>
2022-11-28 12:53:51 -0600

    Mi componente bienvenida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidaComponent } from './Components/bienvenida/bienvenida.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', component:BienvenidaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
