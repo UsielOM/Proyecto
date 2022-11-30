1f192e931530ff440cd7738d23f3c98065d407e6
UsielOM <usiel31197@outlook.es>
2022-11-30 13:46:21 -0600

    Formulario reactivo

--- src/app/Components/registro/registro.component.ts
+++ src/app/Components/registro/registro.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Registro } from 'src/app/interface/registro';
-import { PostService } from 'src/app/service/post.service';
+import { PostService } from 'src/app/Service/post.service';
+
 
 @Component({
   selector: 'app-registro',
@@ -8,8 +10,13 @@ import { PostService } from 'src/app/service/post.service';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+
 reg: Registro = new Registro();
-  constructor(private postService: PostService) { }
+
+  constructor(private postService: PostService, private fb:FormBuilder) {
+    this.formulario();
+   }
+
   registroUsuarios(): void{
     this.postService.crearRegistros(this.reg).subscribe(result => {
       this.reg = result;
@@ -18,6 +25,20 @@ reg: Registro = new Registro();
     })
   }
 
+  form:any;
+
+formulario(){
+  this.form= this.fb.group({
+    Name:['',Validators.required],
+    ApellidoP:['',Validators.required],
+    ApellidoM:['',Validators.required],
+    Correo:['',[Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    Telefono:['',Validators.required],
+    Matricula:['',Validators.required],
+    Area:['',Validators.required],
+    fecha:['',Validators.required]
+  })
+}
   ngOnInit(): void {
   }
 
