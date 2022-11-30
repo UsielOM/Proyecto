51b151dd2f7ad51df48e5281e13e198470f7f392
ReymonYo <Reymon_99@outlook.com>
2022-11-30 13:26:25 -0600

    metodo registro usuarios

--- src/app/Components/registro/registro.component.ts
+++ src/app/Components/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Registro } from 'src/app/interface/registro';
+import { PostService } from 'src/app/service/post.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
-  constructor() { }
+reg: Registro = new Registro();
+  constructor(private postService: PostService) { }
+  registroUsuarios(): void{
+    this.postService.crearRegistros(this.reg).subscribe(result => {
+      this.reg = result;
+      console.log(this.reg);
+      
+    })
+  }
 
   ngOnInit(): void {
   }
